28da734d0dafb149d4c599681d012bb100103eef
WasdexeLee <wasdexe@outlook.com>
2024-04-05 09:07:36 +0800

    added database

--- CapybaraExpress.sql
+++ CapybaraExpress.sql
@@ -1,16 +1,29 @@
-CREATE TABLE Album (
+CREATE TABLE User (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL UNIQUE, 
+    user_password VARCHAR(50) NOT NULL, 
+    user_email VARCHAR(50) NOT NULL,
+    user_address VARCHAR(150) NOT NULL, 
+    user_notification BOOLEAN NOT NULL
+)ENGINE = InnoDB;
 
-    user_name VARCHAR(150) NOT NULL, 
+
+INSERT INTO User 
+VALUES
+(NULL, 'admin', 'admin', 'admin@gmail.com', '99, Jalan 99, Selangor', TRUE),
+(NULL, 'capy', 'loves', 'food@gmail.com', '38, Jalan Capy, Kuala Lumpur', TRUE),
+(NULL, 'zoomer', 'lazyyi', 'zoomer33@gmail.com', '199, Jalan Tun Tan Cheng Lock, 41442, Kuala Lumpur', TRUE),
+(NULL, 'Chef', 'quick22', 'capyba@gmail.com', '20, Jalan KK, 41222, Selangor', FALSE)
 
 
-
-    album_id INT PRIMARY KEY,
-    album_record_label VARCHAR(150) NOT NULL,
-    album_title VARCHAR(150) NOT NULL,
-    album_price REAL NOT NULL,
-    genre_id INT NOT NULL,
-    albumtracks_track_number INT,
-    FOREIGN KEY(genre_id) REFERENCES Genre(genre_id)
+CREATE TABLE Food (
+    food_id INT PRIMARY KEY AUTO_INCREMENT,
+    food_name VARCHAR(100) NOT NULL UNIQUE, 
+    food_category VARCHAR(50) NOT NULL, 
+    food_description VARCHAR(50) NOT NULL,
+    food_price
+    food_availability
+    food_image VARCHAR(150) NOT NULL, 
+    food_prep_time BOOLEAN NOT NULL
 )ENGINE = InnoDB;
 
