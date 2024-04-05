069cb94e9dc7f4fde23108cebb328e836d13b35f
WasdexeLee <wasdexe@outlook.com>
2024-04-05 19:21:02 +0800

    added check password functionality

--- login.php
+++ login.php
@@ -16,15 +16,10 @@ if ($connection->connect_error){
 $query_template = "SELECT user_name, user_password, user_operator FROM User";
 $prepared_query = $connection->prepare($query_template);
 
-// $param = ["song_title", "Song"];
-// $prepared_query->bind_param("ss", $param);
-
-$prepared_query->execute();
-    
-// $result = $prepared_query->get_result();
 $result = [];
 $pass = [];
 
+$prepared_query->execute();
 $prepared_query->bind_result($result[0], $result[1], $result[2]);
 
 while ($prepared_query->fetch()){
@@ -33,7 +28,6 @@ while ($prepared_query->fetch()){
 }
 
 echo json_encode($pass);
-
 
 $prepared_query->close();
 
--- login_script.js
+++ login_script.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let loginDetail = [];
 
+
     fetch('login.php')
     .then(phpResponse => phpResponse.json())
-    .then(table => table.forEach(row => {loginDetail.push(row);
-                                         console.log(row); 
-                                        console.log('yo');}))
+    .then(table => table.forEach(row => loginDetail.push(row)))
+    .then(console.log(loginDetail))
     .catch(error => console.error('Errrooooorrrr: ', error));
 
-    console.log(loginDetail);
+    // console.log(loginDetail);
 
     loginForm.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const password = document.getElementById('password').value;
     
         // Placeholder for authentication logic
-        if (username === 'admin' && password === 'password123') {
+        if (usernameMatch(username, password)) {
             // Redirect to the homepage
             window.location.href = '/homepage.html';
         } else {
@@ -35,7 +35,19 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = '/signup.html';
     });
 
+    
+    function usernameMatch(input_username, input_password){
+        loginDetail.forEach(row => {if (row[0] == input_username) return true;});
 
+        let username_index = loginDetail.findIndex(row => row[0] === input_username);
+
+        console.log(loginDetail[1][1], username_index);
+
+        if ((username_index !== -1) && (loginDetail[username_index][1] === input_password))
+            return true;
+        else 
+            return false;
+    }
 
 
     
