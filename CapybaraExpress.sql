4d202883b41a3a6f0c6d076d78f4caa043d2bcb2
WasdexeLee <wasdexe@outlook.com>
2024-04-06 13:07:05 +0800

    revert login.html script to login and added food table

--- CapybaraExpress.sql
+++ CapybaraExpress.sql
@@ -17,20 +17,41 @@ VALUES
 (NULL, 'Chef', 'quick22', 'capyba@gmail.com', '20, Jalan KK, 41222, Selangor', FALSE, TRUE)
 
 
+CREATE TABLE FoodCategory (
+    food_category_id INT PRIMARY KEY AUTO_INCREMENT, 
+    food_category_name VARCHAR(100) NOT NULL UNIQUE
+)ENGINE = InnoDB;
+
+
+INSERT INTO FoodCategory
+VALUES
+(NULL, 'Fried'),
+(NULL, 'Chicken'),
+(NULL, 'Noodle'),
+(NULL, 'Drinks'),
+(NULL, 'Dessert')
+
+
 CREATE TABLE Food (
     food_id INT PRIMARY KEY AUTO_INCREMENT,
     food_name VARCHAR(100) NOT NULL UNIQUE, 
-    food_category VARCHAR(100) NOT NULL, 
+    food_category_id INT NOT NULL, 
     food_description VARCHAR(300) NOT NULL,
     food_price FLOAT NOT NULL,
     food_availability INT NOT NULL DEFAULT 0,
     food_image VARCHAR(150) NOT NULL, 
-    food_prep_time TIME NOT NULL,
-    food_num_sold INT NOT NULL DEFAULT 0
+    food_prep_time INT NOT NULL,
+    food_num_sold INT NOT NULL DEFAULT 0,
+    FOREIGN KEY(food_category_id) REFERENCES FoodCategory(food_category_id)
 )ENGINE = InnoDB;
 
 
 INSERT INTO Food
 VALUES
-(NULL, )
-
+(NULL, 'Drumstick Fried Chikin Combo', 1, 'Drumstick Korean Fried Chikin (2pcs) Seasoned With One Type of Korean Sauces Served Together With Kimchi French Fries and Kimchi', 18.9, 10, 'food-image/Drumstick Fried Chikin Combo.webp', 30, DEFAULT),
+(NULL, 'Soy Garlic (Authentic Korean Flavor)', 1, 'Korean Fried Chicken Only The crispy and tender chicken is coated in a luscious glaze that is both sweet and spicy, creating a delightful taste sensation.', 15.9, 10, 'food-image/Soy Garlic (Authentic Korean Flavor).webp', 30, DEFAULT),
+(NULL, 'Tau Fu Fah', 5, 'I Love to Eat TOFFUUUU', 5.9, 10, 'food-image/Tau Fu Fah.webp', 25, DEFAULT),
+(NULL, 'Peach Gum Snow Fungus with Longan', 5, 'Nice', 8.9, 10, 'food-image/Peach Gum Snow Fungus with Longan.webp', 15, DEFAULT),
+(NULL, 'Tandoori Chicken Rice Half', 2, 'You wanna eat my chicken', 44.9, 10, 'food-image/Tandoori Chicken Rice Half.webp', 20, DEFAULT),
+(NULL, 'Pan Mee Soup', 3, 'Send N00ds', 11.4, 10, 'food-image/Pan Mee Soup.webp', 22, DEFAULT),
+(NULL, 'Iced Latte', 4, 'Freshly brewed Iced Latte using 100% Arabica coffee beans and fresh milk.', 8.8, 10, 'food-image/Iced Latte.webp', 38, DEFAULT)
